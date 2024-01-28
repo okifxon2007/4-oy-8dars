ce207ada0d1f0aa2ec90211d671437b2098302b9
okifxon <shokirjonovokifxon46@gmail.com>
2024-01-28 14:10:55 +0300

    dars

---
+++ style.js
@@ -0,0 +1,149 @@
+//kubning yon uzunligini xisobga olgan xolda uning hajmini xisoblaydigan fuknsiya yozing
+
+// function kub(arg) {
+//     arg *= 12
+//     return arg
+    
+// }
+// console.log(kub(4));
+
+//satrdagi malum bir belgi mavjudligini tekshiradigan fuksiya aniqlang 
+
+
+// massivda birinchi natural sonlarni qaytaradigan funksiya yarating
+
+// function natural(arg) {
+//     if (arg <= 9 ) {
+//         result = "bu son natural son"
+        
+//     }else{
+//         result = "bu son natural son emas"
+//     }
+//     return result
+// }
+// console.log(natural(11));
+
+//raqamdagi raqamlar yigindisini xisoblaydigan funksiya yozing
+
+// function yigindi(arg1) {
+//     a = Math.floor (arg1 / 100)
+//     b = Math.floor (arg1 / 10 % 10)
+//     c = Math.floor (arg1 % 10)
+//     result = a + b + c
+//     return result
+    
+// }
+// console.log(yigindi(555));
+
+//bir yil kabisa yili ekanligini tekshiradigan funksiya yozing
+// function kabisaYiliTekshir(year) {
+//     if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// // Test qilingan yillar
+// let testYillar = [2000, 2004, 2008, 2012, 2016, 2020, 2024];
+
+// testYillar.forEach(yil => {
+//     if (kabisaYiliTekshir(yil)) {
+//         console.log(yil + " - Kabisa yili");
+//     } else {
+//         console.log(yil + " - Kabisa yili emas");
+//     }
+// });
+
+//ikki berilgan qadriyatlar tasodifiy raqamni xosil qilish vazifasini yaratish 
+
+// tortburchakning kengligi va balandligini xisobga olgan xolda uning maydonini xisoblaydigan funksiya yozing
+
+//ferengeytni selsiyga aylantiradigan funksiya yozing
+
+// function selsiy(farengeyt) {
+//     let max = Math.trunc(farengeyt-32) / 1.8;
+//     return max
+// }
+// console.log(selsiy(234));
+
+//uch burchakning teng qirrali teng yonli yoki skalen ekanligini tekshiradigan funksiya yozing 
+// function uchburchak(a,b,c) {
+//     if (a == b && a==c && b==c) {
+//         result = "teng qirrali"
+//     }else{
+//         result = "skalen"
+//     }
+//     return result 
+// }
+// console.log(uchburchak(10,10,1));
+
+//ikkita sonning minimalini topadigan funksiyani ishlab chiqing 
+// function minimal(min,max) {
+//     if (min <= max) {
+//         result = min
+//     }else{
+//         result = max
+//     }
+//     return result
+// }
+// console.log(minimal(33,56));
+//radius berilgan aylananing maydonini xisoblaydigan funksiya yozing 
+// function maydonTop(radius) {
+//     let p = 3
+//     let maydon = p * radius * radius
+//     return maydon
+// }
+// console.log(maydonTop(8));
+
+//agar maglubiyat bir palindrom bolsa tekshiradi vazifasini yaratish 
+// function palindrom(a) {
+//     result = Math.floor (a % 10)
+//     result = Math.floor (a % 10 / 10)
+//     result = Math.floor (a % 100)
+//     return result
+    
+// }
+// console.log(palindrom (121));
+
+//silindir xajmini uning radiusi va balandligini xisobga olgan xolda xisoblaydigan funksiyani ishlab chiqing 
+// function silindr(radius,height) {
+//     return (radius**2)*height
+// }
+// console.log(silindr(4,6));
+
+// nuqta tortburchak ichida yotishini aniqlovchi funksiya ishlab chiqing
+
+//uchburchakning yon uzunliklarini xisobga olgan holda uning perimetrini xisoblaydigan funksiyani ishlab chqing
+
+//raqamning toq mukammal kvadrat ekanligini aniqlovchi funksiya yozing 
+
+// belgilangan ikkitasi ortasidagi tasodifiu juft sonni xosil qiladigan funksiyani yarating 
+
+// function son(son1,son2) {
+//     if (son1 % 2 == 0) {
+//         result = 'juft son'
+//     }else{
+//         result = 'juft son emas'
+//     }
+//     return result
+// }
+// console.log(son(10));
+
+// konusning radiusi va balandligini xisobga olgan xolda uning xajmini xisoblaydigan funksiya yozing 
+
+// function cone(radius,height) {
+//     return ((1/3)*(radius**2))*height
+// }
+// console.log(cone(4,9));
+
+// string togri telefon raqam ekanligini tekshiradigan funksiyani aniqlang 
+
+
+
+
+
+
+
+
+
